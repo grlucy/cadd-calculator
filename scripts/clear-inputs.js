25ecf11c382b6d69ffa53c688b308c5fcc76d337
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2025-01-22 11:17:45 -0500

    refactor inputs

--- scripts/clear-inputs.js
+++ scripts/clear-inputs.js
@@ -1,12 +1,6 @@
-function clearInputs() {
-  const decFtInput = document.getElementById("decFtFrm-decFtInput");
-  decFtInput.value = "";
-  const ftInput = document.getElementById("decFtFrm-ftInput");
-  ftInput.value = "";
-  const inInput = document.getElementById("decFtFrm-inInput");
-  inInput.value = "";
-  const numInput = document.getElementById("decFtFrm-frcInput1");
-  numInput.value = "";
-  const denInput = document.getElementById("decFtFrm-frcInput2");
-  denInput.value = "";
+function clearInputsById(idArray) {
+  idArray.forEach((idStr) => {
+    const input = document.getElementById(idStr);
+    input.value = "";
+  });
 }
